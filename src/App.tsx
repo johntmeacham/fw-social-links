70bd01918eda24fc57d70d1942a43a081e511220
johntmeacham <john@ignitecloud.io>
2024-04-18 15:32:43 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.scss';
+import SocialLink from './components/SocialLink.tsx';
+
+const socialLinks = [
+  { site: 'GitHub', url: '#' },
+  { site: 'Frontend Mentor', url: '#' },
+  { site: 'LinkedIn', url: '#' },
+  { site: 'Twitter', url: '#' },
+  { site: 'Instagram', url: '#' },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <main>
+      <div className="profile-card">
+        <img
+          className="profile-card__avatar"
+          alt="Jessica Randall"
+          src="images/avatar-jessica.jpeg"
+        ></img>
+        <h2 className="profile-card__name">Jessica Randall</h2>
+        <p className="profile-card__location">London, United Kingdom</p>
+        <p className="profile-card__bio">
+          "Front-end developer and avid reader."
+        </p>
+        <div className="profile-card__links">
+          {socialLinks.map((link, index) => (
+            <SocialLink key={index} site={link.site} url={link.url} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
